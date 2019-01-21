dd4a9e10c4330f2b5f30c635e379f4960909cad9
Freja Petersen <Freja@frejas-mbp-4.eduroam.wireless.dtu.dk>
2019-01-21 11:52:26 +0100

    rjkdn

--- dataCheck.py
+++ dataCheck.py
@@ -29,7 +29,11 @@ def dataCheck(data):
     #Define a vector with the grading scale:
     gradeScale = np.array([-3,0,2,4,7,10,12])
     
-    grades = np.array(datagrade)
+        #Counting number of columns
+    numcol = len(data[0,:])
+    
+    #Adding all of the grades to one dataframe
+    grades = (data[:,2:numcol].astype(int)) 
     
     #Define length of matrix containing all the grades 
     n = len(grades)
@@ -39,13 +43,9 @@ def dataCheck(data):
         #Only checks the columns that contains grades (numcol-2)
         for j in range((numcol-2)):
             #If the grade is within the Scale, inserts a true
-            if np.any(grades[i,j] == gradeScale):
-                grades[i,j] = True
-                
-            #If the grade is not within the scale, inserts false.
-            else:
-                grades[i,j] = False
-               
+            if not np.any(grades[i,j] == gradeScale):
                 #Prints the name and student ID of the student that has an incorect grade according to the grading scale
                 #plus the assignment in which the mistake occurs. 
                 print("{:s}, {:s} has an incorect grade in assignment {}.".format(data[i,1],data[i,0],j+1))
+
+                
