7ce340e3223e1d7532a775375b8bb80e686b7e16
Kasper <kasper.telkamp@gmail.com>
2019-01-21 13:52:37 +0100

    Rettet med funktion

--- dataCheck.py
+++ dataCheck.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 
 from dataLoad import *
+from dataToGrades import dataToGrades
 
 
 def dataCheck(data):
@@ -28,12 +29,9 @@ def dataCheck(data):
 
     #Define a vector with the grading scale:
     gradeScale = np.array([-3,0,2,4,7,10,12])
-    
-        #Counting number of columns
-    numcol = len(data[0,:])
-    
-    #Adding all of the grades to one dataframe
-    grades = (data[:,2:numcol].astype(int)) 
+     
+    #We call the function to convert the data into only grades.
+    grades = dataToGrades(data)
     
     #Define length of matrix containing all the grades 
     n = len(grades)
@@ -41,22 +39,12 @@ def dataCheck(data):
     #Create a for-loop that checks if the grade givin is contained in the grading scale:
     for i in range(n):
         #Only checks the columns that contains grades (numcol-2)
-        for j in range((numcol-2)):
+        for j in range(len(grades[0,:])):
             #If the grade is within the Scale, inserts a true
 
             if not np.any(grades[i,j] == gradeScale):
-
-            if np.any(grades[i,j] == gradeScale):
-                grades[i,j] = True
-                print("We found no errors in the given grades.")
-                
-            #If the grade is not within the scale, inserts false.
-            else:
-                grades[i,j] = False
-               
-
                 #Prints the name and student ID of the student that has an incorect grade according to the grading scale
                 #plus the assignment in which the mistake occurs. 
-                print("{:s}, {:s} has an incorect grade in assignment {}.".format(data[i,1],data[i,0],j+1))
-
+                print("{:s}, {:s} has an incorrect grade in assignment {}.".format(data[i,1],data[i,0],j+1))
+    
                 
