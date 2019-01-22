f6c7378a0e7c23be6bcc5981a32d0997642f7466
Freja Petersen <Freja@frejas-mbp-4.eduroam.wireless.dtu.dk>
2019-01-22 13:18:52 +0100

    iujf

--- Mandag/dataLoad.py
+++ Mandag/dataLoad.py
@@ -11,17 +11,7 @@ def dataLoad(filename):
     
     """
     
-    
     #Reading data
     data = pd.read_csv(filename)
     
-    #Using argsort to get index from sorted values. 
-    index = data.Name.values.astype(str).argsort()
-    #Sorting, using the indices
-    #https://stackoverflow.com/questions/43401903/python-order-dataframe-alphabetically
-    data = pd.DataFrame(data.values[index], data.index[index], data.columns)
-    
-    data = np.array(data)
-    
-    print("There is a total number of {} students in the loaded data, and the number of graded assignments is {}.\n".format(len(data), len(data[0,:])-2))
     return(data)
