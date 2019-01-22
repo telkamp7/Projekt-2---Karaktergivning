9987a05ea82624d6cf63a44aea3598175d17cabd
Freja Petersen <Freja@frejas-mbp-4.eduroam.wireless.dtu.dk>
2019-01-22 14:04:09 +0100

    ojfk

--- Mandag/computeFinalGrades.py
+++ Mandag/computeFinalGrades.py
@@ -1,9 +1,6 @@
 import numpy as np
 from roundGrade import roundGrade
 
-#   Tests
-#grades = np.array([[4, 0, 4, 7], [7, -3, 12, 10], [2, 0, -3, 4]])
-#grades = np.array([[4],[7],[12]])
 
 def computeFinalGrades(grades):
     
@@ -15,35 +12,26 @@ def computeFinalGrades(grades):
     
     #   If theres is one assignment, the gradesFinal is equal to grades.
     if M == 1:
-        for i in range(len(grades[:,0])):
+        for i in range(len(grades)):
             gradesFinal = np.append(gradesFinal, grades[i,0]).astype(int)
             
     #   If there is more than one assignment, the gradesFinal is equal to
-    #   the mean value of the grades, minus the minimum grade, rounded with
+    #   the mean value of the grades (minus the minimum grade) rounded with
     #   the roundGrade function.
     if M > 1:
         
-        for j in range(len(grades[:,0])):
+        for j in range(len(grades)):
             if np.any(grades[j,:] == -3):
                 grades[j,:] = -3
-                    
         
-        
-        #   We create a mask to remove the minimum grade for each student.
-        rem_minimum_grades = np.ma.masked_where(grades == np.resize(grades.min(axis = 1),[grades.shape[0],1]), grades)
-        
-        #   We calculate the mean grade, minus the minimum grade, for each student.
-        studentMean = np.ma.mean(rem_minimum_grades, axis = 1)
-        
-        #   If a student receives -3 in atleast one assignement, the gradesFinal
-        #   must return -3
-        for m in range(len(grades[:,0])):
-            if grades[m,0] == -3:
-                studentMean[m] = -3
-        
+        #Calculate mean values.
+        studentMean = np.array([])        
+        for i in range(len(grades)):
+            minval = np.min(grades[i,:])
+            studentMean = np.append(studentMean, (np.sum(grades[i,:]) - minval) / (M - 1))
+            
         #   We calculate the final grade for each student using the roundGrade function.
         gradesFinal = roundGrade(studentMean)
     
     return(gradesFinal)
     
-#print(computeFinalGrades(grades))
