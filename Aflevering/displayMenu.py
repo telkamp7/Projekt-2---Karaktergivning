cb481809bdd9585095cb20bd59523af20d966b83
Anna <s174349@student.dtu.dk>
2019-01-23 10:37:08 +0100

    jij

--- Aflevering/displayMenu.py
+++ Aflevering/displayMenu.py
@@ -1,34 +1,32 @@
+  """
+Dette script er lånt fra undervisningsmaterialet 'modules_python.pdf' 
+side 64 men indeholder betydelige ændringer.
 
+The whole assignment has been worked out by the whole group in collaboration,
+but it has been divided into sections, each with a responsible group member.
+   
+RESPONSIBLE: Anna Sophie Bjerremand Jensen, s174349.
+"""
+
+"""
+DISPLAYMENU Displays a menu of options, ask the user to choose an item
+and returns the number of the menu item chosen.
+
+Usage: choice = displayMenu(options)
+
+Input    options   Menu options (array of strings)
+Output   choice    Chosen option (integer)
+
+Author: Mikkel N. Schmidt, mnsc@dtu.dk, 2015
+"""
+    
 import numpy as np
-from inputNumber import inputNumber
+from inputNumber import *
 
-
+#
 
 def displayMenu(options):
-    """
-    This script is from the material used in the lectures 'modules_python.pdf' 
-    page 64 but a few changes has been added. 
-    
-    
-    Author: Mikkel N. Schmidt, mnsc@dtu.dk, 2015
-    
-    This assignment has been created 
-    ###Opgaven er udelukkende udarbejdet i fælleskab.###
-    RESPONSIBLE: Anna Sophie Bjerremand Jensen, s174349.
-        
-    """
-    
-    """
-    DISPLAYMENU Displays a menu of options, ask the user to choose an item
-    and returns the number of the menu item chosen.
-    
-    Usage: choice = displayMenu(options)
-    
-    Input    options   Menu options (array of strings)
-    Output   choice    Chosen option (integer)
-    
-    Author: Mikkel N. Schmidt, mnsc@dtu.dk, 2015
-    """
+  
     
     # Display menu options
     for i in range(len(options)):
@@ -36,9 +34,19 @@ def displayMenu(options):
         print("{:d}. {:s}".format(i+1, options[i]))
     #   Set choice = 0 
     choice = 0
-    #   If the input number is equal to one of the options numbers:
-    while not(np.any(choice == np.arange(len(options))+1)):
-        #   Prints a message for the user
+    #   If the input number is equal to one of the options numbers
+    
+    #This while loop has been crafted by the group. 
+    while True:
+        #Prompts the user for an input as a number from the main menu
         choice = inputNumber("Please, choose a number from the options: ")
+        
+        # if the input isn´t a valid number from the menuoptions, an error-message is printed, and the user is asked for a new input 
+        if not np.any(choice == np.arange(len(options)+1)):
+            print("\nAn error occurred. Make sure to input a number from the menu.")
+            pass
+        
+        else:
+            break
         
     return choice
